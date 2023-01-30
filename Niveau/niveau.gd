e4c04d38867e9990e6e1e03068f7743966e0d6f5
corentin brulé <corentin.brule@gmail.com>
2023-01-30 14:14:18 +0100

    reset message "win" quand restart_level

--- Niveau/niveau.gd
+++ Niveau/niveau.gd
@@ -49,12 +49,12 @@ func restart_level():
 	add_child_below_node($TileMap,new_tileMap)
 	$TileMap.free()
 	new_tileMap.set_name(name)
-	print("restart")
+	
+	$"GUI/%win".hide()
+	
+	# restart Avatar
 	print($Avatar.position)
-	#$Avatar.position = start_position
 	$Avatar.life()
-	#$Avatar.position = start_position
-	print($Avatar.position)
 
 func clean_death_marks():
 	for mark in death_marks:
