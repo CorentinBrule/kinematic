75d84df2445b716bf9782ced7db582edf2385748
corentinbrule <corentin.brule@gmail.com>
2023-02-16 13:27:44 +0100

    close #3

--- Global.gd
+++ Global.gd
@@ -41,7 +41,7 @@ func _ready():
 	current_scene = root.get_child(root.get_child_count() - 1)
 	
 	save_files_path = list_files_in_directory(save_folder_path)
-	save_files_data = load_files_in_directory(save_folder_path)
+	save_files_data = load_files(save_files_path)
 	if current_scene.has_node("Menu"):
 		current_scene.get_node("Menu").init(save_files_data)
 
@@ -124,42 +124,28 @@ func list_files_in_directory(path):
 	var dir = Directory.new()
 	dir.open(path)
 	dir.list_dir_begin()
-
+	
 	while true:
 		var file = dir.get_next()
 		if file == "":
 			break
 		elif not file.begins_with("."):
 			files.append(path + "/" + file)
-
-	dir.list_dir_end()
+	
+	files.sort()
 	return files
 
-
-
-func load_files_in_directory(path):
+func load_files(files):
 	var datas=[]
-	var dir = Directory.new()
-	dir.open(path)
-	dir.list_dir_begin()
-	
-	while true:
-		#print("-------")
-		var file_path = dir.get_next()
-		#print(file_path)
+	for file_path in files:
 		if file_path == "":
 			break
 		elif not file_path.begins_with("."):
 			var file = File.new()
-			file_path = path + "/" + file_path
-			#print(file_path)
 			if file.file_exists(file_path):
 				file.open(file_path, file.READ)
 				var data_dict = parse_json(file.get_as_text())
 				data_dict["file_path"] = file_path
 				datas.append(data_dict)
 				file.close()
-				
-	dir.list_dir_end()
-
 	return datas
