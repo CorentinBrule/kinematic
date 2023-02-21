0295d7d9017fed27374b291f49ac39639b9c692b
corentinbrule <corentin.brule@gmail.com>
2023-02-21 16:34:11 +0100

    ajout du mode autocam, correction taille des Clips (Cherry Pick 24897bb)

--- Niveau/CameraController.gd
+++ Niveau/CameraController.gd
@@ -1,5 +1,6 @@
 extends Camera2D
 
+@export var auto_cam:bool = false
 @export var joystick_cam:bool = false
 
 var avatar
@@ -22,18 +23,32 @@ var clip_open_right = false
 var zooming = false
 
 func _ready():
-	# avatar = get_parent().get_node("Avatar")
-	zoom_val = 0.5
-	print("val: "+str(zoom_val)+" zoom: " + str(zoom))
+	avatar = get_parent().get_node("Avatar")
+	init()
 	clip_left_position_dest = $ClipGauche.position.x
 	clip_left_width_dest = $ClipGauche.size.x
 	clip_right_position_dest = $ClipDroite.position.x
 	clip_right_width_dest = $ClipDroite.size.x
-	
+
 func init():
-	zoom_val = 0.5
+	if !auto_cam:
+		zoom_val = 0.5
+	else:
+		zoom_val = 1
+		zoom_dest = min_zoom
+		zoom = min_zoom
+	$ClipGauche.position.x = get_viewport().size.x * -1
+	$ClipDroite.position.x = get_viewport().size.x
+	adapt_clip_destination()
 
 func _input(event):
+	if auto_cam:
+		if event.is_action("move_bottom") or event.is_action("move_right") or event.is_action("move_left") or event.is_action("move_up") or event.is_action("Sauter"):
+			if event.get_action_strength("move_bottom") > 0.5 or event.get_action_strength("move_right") > 0.5 or event.get_action_strength("move_left") > 0.5 or event.get_action_strength("move_up") > 0.5 or event.get_action_strength("Sauter") > 0.5:
+				# il y a un truc que j'ai pas compris ave les zones mortes des joysticks...
+				reset_zoom()
+				open_clip_controls()
+	
 	if event.is_action("zoom_up_mouse"):
 		zoom_in()
 	if event.is_action("zoom_down_mouse"):
@@ -46,7 +61,7 @@ func _input(event):
 			reset_zoom()
 
 func _process(delta):
-
+	
 	if zoom_val < 0.5: 
 		target_pos = lerp(avatar.position, middle_pos, zoom_val) 
 	else: 
@@ -64,19 +79,16 @@ func _process(delta):
 		open_clip()
 	else:
 		clip_open = false
-
+	
 	if zoom.distance_to(zoom_dest) > 0.005:
 		zooming = true
 		position = lerp(position, target_pos, 0.1)
 	else: 
 		zooming = false
 		position = target_pos
-		
+	
 	zoom = lerp(zoom,zoom_dest,0.05)
-	$ClipGauche.position.x = lerpf($ClipGauche.position.x, clip_left_position_dest, 0.05)
-	$ClipGauche.size.x = lerpf($ClipGauche.size.x, clip_left_width_dest, 0.05)
-	$ClipDroite.position.x = lerpf($ClipDroite.position.x, clip_right_position_dest, 0.05)
-	$ClipDroite.size.x = lerpf($ClipDroite.size.x, clip_right_width_dest, 0.05)
+	update_clip()
 
 func zoom_in(zoom_speed_changer=1):
 	zoom_val = max(zoom_val - zoom_speed/zoom_speed_changer, 0)
@@ -107,7 +119,6 @@ func reset_zoom():
 	target_pos = middle_pos
 	adapt_clip_destination()
 #	print("val: "+str(zoom_val)+" zoom_dest: " + str(zoom_dest))
-	
 
 func adapt_clip():
 	adapt_clip_destination()
@@ -127,6 +138,12 @@ func adapt_clip_destination():
 		clip_left_width_dest =  edge_from_center * zoom_factor - 192 + 5
 		clip_right_position_dest = 192 - 5
 		clip_right_width_dest = edge_from_center * zoom_factor - 192 + 5
+
+func update_clip():
+	$ClipGauche.position.x = lerpf($ClipGauche.position.x, clip_left_position_dest, 0.05)
+	$ClipGauche.size.x = lerpf($ClipGauche.size.x, clip_left_width_dest, 0.05)
+	$ClipDroite.position.x = lerpf($ClipDroite.position.x, clip_right_position_dest, 0.05)
+	$ClipDroite.size.x = lerpf($ClipDroite.size.x, clip_right_width_dest, 0.05)
 
 func force_update_clip():
 	$ClipGauche.position.x = clip_left_position_dest
