CorentinBrule/kinematic

commit 258f9f5f48d60ad940a25551c58bb1c2ca39ad35

certaines actions tactiles à gauche quand y'en a trop, jolie reveal quand le clap se ferme

tools.gd
@@ -61,15 +61,18 @@ func _change_mobile_emulation(val):
 	has_touch_screen = val
 	var action_caintainer_keyboard = get_node("Niveau/GUI/outGameGUI/HBoxContainer_droit/VBoxContainer/ActionsContainer")
 	var action_container_touch_droit = get_node("Niveau/GUI/outGameGUI/HBoxContainer_droit/VBoxContainer/ActionsContainerTouch")
+	var action_container_touch_overflow = get_node("Niveau/GUI/outGameGUI/HBoxContainer_gauche/VBoxContainer/ActionsContainerTouchOverflow/ActionsContainerTouch2")
 	var main_touch_controls = get_node("Niveau/GUI/touch_controls")
 	if Engine.editor_hint:
 		ProjectSettings.set_setting("input_devices/pointing/emulate_touch_from_mouse",val)
 		ProjectSettings.save()
 		if val:
 			action_container_touch_droit.show()
+			action_container_touch_overflow.show()
 			main_touch_controls.show()
 			action_caintainer_keyboard.hide()
 		else:
+			action_container_touch_overflow.hide()
 			action_container_touch_droit.hide()
 			main_touch_controls.hide()
 			action_caintainer_keyboard.show()

Niveau/CameraController.gd
@@ -158,7 +158,7 @@ func adapt_clips():
 		
 		clip_left_position_dest = (edge_from_center * -1 * zoom_dest.x) - 5		
 		if clip_open_left == false:
-			clip_left_width_dest =  (edge_from_center * zoom_dest.x - 192) + 5
+			clip_left_width_dest =  (edge_from_center * zoom_dest.x - 192) + 20
 		else:
 			clip_left_width_dest = 0
 		
@@ -169,10 +169,12 @@ func adapt_clips():
 			clip_right_position_dest = 192 + clip_right_width_dest
 
 func update_clip():
-	if(abs($ClipGauche.rect_position.x - clip_left_position_dest) > 0.5):
-		$ClipGauche.rect_position.x = lerp($ClipGauche.rect_position.x, clip_left_position_dest, 0.05)
 	if(abs($ClipGauche.rect_size.x - clip_left_width_dest) > 0.5):
 		$ClipGauche.rect_size.x = lerp($ClipGauche.rect_size.x, clip_left_width_dest, 0.05)
+		$"%Reveal_actions_touch_overflow".scale.x = lerp($ClipGauche.rect_size.x, clip_left_width_dest, 0.05)
+	if(abs($ClipGauche.rect_position.x - clip_left_position_dest) > 0.5):
+		$ClipGauche.rect_position.x = lerp($ClipGauche.rect_position.x, clip_left_position_dest, 0.05)
+		$"%Reveal_actions_touch_overflow".position.x = $ClipGauche.rect_position.x + ($"%Reveal_actions_touch_overflow".scale.x/2)
 	if(abs($ClipDroite.rect_position.x - clip_right_position_dest) > 0.5):
 		$ClipDroite.rect_position.x = lerp($ClipDroite.rect_position.x, clip_right_position_dest, 0.05)
 	if(abs($ClipDroite.rect_size.x - clip_right_width_dest) > 0.5):

Niveau/GUI/GUI.gd
@@ -10,6 +10,7 @@ var out_game_meta
 var out_game_bas_droit
 var actions_container
 var actions_touch_container
+var actions_touch_container_overflow
 
 var avatar
 var color
@@ -29,6 +30,7 @@ func _ready():
 	out_game_bas_droit = $"%bas_droit"
 	actions_container = $"%ActionsContainer"
 	actions_touch_container = $"%ActionsContainerTouch"
+	actions_touch_container_overflow = $"%ActionsContainerTouch2"
 	adapt_interface()
 
 func init():
@@ -42,7 +44,11 @@ func init():
 		action.free()
 	for action in actions_touch_container.get_children():
 		action.free()
+	for action in actions_touch_container_overflow.get_children():
+		action.free()
 	
+	var max_height_touch = actions_touch_container.rect_size.y
+	var index_action = 0
 	for item in avatar.get_active_items():
 		#print(item)
 		var gui_action = action_scene.instance()
@@ -52,11 +58,16 @@ func init():
 		
 		var gui_action_touch = action_scene_touch.instance()
 		gui_action_touch.init(item)
-		actions_touch_container.add_child(gui_action_touch)
-		actions_touch_container.move_child(gui_action_touch, 0)
+
+		if index_action < 5:
+			actions_touch_container.add_child(gui_action_touch)
+			actions_touch_container.move_child(gui_action_touch, 0)
+		else:
+			actions_touch_container_overflow.add_child(gui_action_touch)
+		index_action += 1
 	
 	update_interface()
-	
+
 	if get_parent().get_parent().has_node("Menu"):
 		$"%retourMenu".visible = true
 		
@@ -109,7 +120,7 @@ func update_interface():
 		actions_container.show()
 		$"%GuiActionZoom".show()
 		$"%bas_droit/MarginContainer".hide()
-
+		
 func adapt_interface():
 	if not Engine.editor_hint:
 		var resize_ratio = get_viewport().size.x / get_viewport().size.y

Niveau/GUI/GUI.tscn
@@ -1,4 +1,4 @@
-[gd_scene load_steps=11 format=2]
+[gd_scene load_steps=14 format=2]
 
 [ext_resource path="res://assets/FT88-regular.tres" type="DynamicFont" id=1]
 [ext_resource path="res://Niveau/GUI/GUI_actions.tscn" type="PackedScene" id=2]
@@ -18,6 +18,9 @@ default_font = ExtResource( 1 )
 [sub_resource type="ShaderMaterial" id=14]
 script = SubResource( 13 )
 
+[sub_resource type="CanvasItemMaterial" id=16]
+light_mode = 2
+
 [node name="GUI" type="Node2D"]
 z_index = 10
 script = ExtResource( 3 )
@@ -30,7 +33,6 @@ unique_name_in_owner = true
 visible = false
 
 [node name="outGameGUI" type="Control" parent="."]
-anchor_right = 0.219
 margin_right = 384.0
 margin_bottom = 384.0
 grow_horizontal = 2
@@ -53,10 +55,11 @@ align = 1
 anchor_left = -1.0
 anchor_bottom = 1.0
 margin_left = -7.0
+margin_bottom = 202.0
 
 [node name="VBoxContainer" type="VBoxContainer" parent="outGameGUI/HBoxContainer_gauche"]
 margin_right = 391.0
-margin_bottom = 384.0
+margin_bottom = 586.0
 size_flags_horizontal = 3
 
 [node name="char_name" type="Label" parent="outGameGUI/HBoxContainer_gauche/VBoxContainer"]
@@ -244,7 +247,7 @@ margin_bottom = 25.0
 unique_name_in_owner = true
 margin_top = 88.0
 margin_right = 1005.0
-margin_bottom = 110.0
+margin_bottom = 113.0
 grow_horizontal = 0
 alignment = 2
 
@@ -271,3 +274,25 @@ margin_left = 734.0
 margin_right = 1005.0
 margin_bottom = 25.0
 text = "pour revenir au menu"
+
+[node name="ActionsContainerTouchOverflow" type="Node2D" parent="outGameGUI"]
+light_mask = 3
+position = Vector2( 0, 384 )
+z_index = 22
+z_as_relative = false
+
+[node name="ActionsContainerTouch2" type="VBoxContainer" parent="outGameGUI/ActionsContainerTouchOverflow"]
+unique_name_in_owner = true
+visible = false
+light_mask = 2
+material = SubResource( 16 )
+anchor_left = -1.0
+anchor_right = -1.0
+margin_left = -191.0
+margin_top = -165.0
+margin_right = -1.0
+margin_bottom = -115.0
+grow_horizontal = 0
+grow_vertical = 0
+
+[node name="Gui_actions_touch" parent="outGameGUI/ActionsContainerTouchOverflow/ActionsContainerTouch2" instance=ExtResource( 8 )]

Niveau/GUI/GUI_actions_touch.tscn
@@ -79,6 +79,7 @@ tracks/1/keys = {
 }
 
 [node name="GUI_actions_touch" type="HBoxContainer"]
+use_parent_material = true
 margin_right = 190.0
 margin_bottom = 50.0
 rect_min_size = Vector2( 0, 50 )
@@ -89,6 +90,7 @@ __meta__ = {
 }
 
 [node name="ActionDescription" type="Label" parent="."]
+use_parent_material = true
 margin_right = 190.0
 margin_bottom = 50.0
 rect_min_size = Vector2( 190, 50 )
@@ -100,6 +102,7 @@ valign = 1
 autowrap = true
 
 [node name="TouchButton" type="TouchScreenButton" parent="ActionDescription"]
+use_parent_material = true
 position = Vector2( 117, -21 )
 scale = Vector2( 0.29, 0.29 )
 z_index = -1
@@ -111,6 +114,7 @@ anchor_bottom = 1.0
 color = Color( 0, 0, 0, 0.501961 )
 
 [node name="TextureProgress" type="TextureProgress" parent="ActionDescription/TouchButton"]
+use_parent_material = true
 anchor_right = 1.0
 anchor_bottom = 1.0
 value = 100.0
@@ -120,6 +124,7 @@ tint_progress = Color( 1, 1, 1, 0.470588 )
 nine_patch_stretch = true
 
 [node name="TextureCooldown" type="TextureProgress" parent="ActionDescription/TouchButton"]
+use_parent_material = true
 anchor_right = 1.0
 anchor_bottom = 1.0
 texture_progress = ExtResource( 5 )
@@ -129,6 +134,7 @@ tint_progress = Color( 0.419608, 0.419608, 0.419608, 0.470588 )
 nine_patch_stretch = true
 
 [node name="TextureRect" type="TextureRect" parent="ActionDescription/TouchButton"]
+use_parent_material = true
 margin_left = -99.758
 margin_right = 300.242
 margin_bottom = 300.0

Niveau/Niveau.tscn
@@ -1,8 +1,9 @@
-[gd_scene load_steps=9 format=2]
+[gd_scene load_steps=10 format=2]
 
 [ext_resource path="res://Niveau/niveau.gd" type="Script" id=1]
 [ext_resource path="res://Niveau/Avatar/light.png" type="Texture" id=2]
 [ext_resource path="res://Niveau/trigger_end.gd" type="Script" id=3]
+[ext_resource path="res://assets/1px_texture.png" type="Texture" id=4]
 [ext_resource path="res://Niveau/CameraController.gd" type="Script" id=12]
 [ext_resource path="res://Niveau/GUI/GUI.tscn" type="PackedScene" id=13]
 
@@ -21,14 +22,19 @@ __meta__ = {
 "_edit_lock_": true
 }
 
-[node name="GUI" parent="." instance=ExtResource( 13 )]
-
 [node name="Camera2D" type="Camera2D" parent="."]
 pause_mode = 1
 position = Vector2( 192, 192 )
 z_index = 20
 current = true
 script = ExtResource( 12 )
+
+[node name="Reveal_actions_touch_overflow" type="Light2D" parent="Camera2D"]
+unique_name_in_owner = true
+light_mask = 2
+position = Vector2( -359.441, 56 )
+scale = Vector2( 337.117, 714 )
+texture = ExtResource( 4 )
 
 [node name="ClipGauche" type="ColorRect" parent="Camera2D"]
 material = SubResource( 10 )
@@ -46,6 +52,8 @@ margin_top = -500.0
 margin_right = 192.0
 margin_bottom = 500.0
 color = Color( 0, 0, 0, 1 )
+
+[node name="GUI" parent="." instance=ExtResource( 13 )]
 
 [node name="Trigger_end" type="Area2D" parent="."]
 position = Vector2( 32, 32 )

assets/1px_texture.png.import
@@ -1,0 +1,35 @@
+[remap]
+
+importer="texture"
+type="StreamTexture"
+path="res://.import/1px_texture.png-e647f5d966473c9d47d621a561c9ac61.stex"
+metadata={
+"vram_texture": false
+}
+
+[deps]
+
+source_file="res://assets/1px_texture.png"
+dest_files=[ "res://.import/1px_texture.png-e647f5d966473c9d47d621a561c9ac61.stex" ]
+
+[params]
+
+compress/mode=0
+compress/lossy_quality=0.7
+compress/hdr_mode=0
+compress/bptc_ldr=0
+compress/normal_map=0
+flags/repeat=0
+flags/filter=true
+flags/mipmaps=false
+flags/anisotropic=false
+flags/srgb=2
+process/fix_alpha_border=true
+process/premult_alpha=false
+process/HDR_as_SRGB=false
+process/invert_color=false
+process/normal_map_invert_y=false
+stream=false
+size_limit=0
+detect_3d=true
+svg/scale=1.0

commit 536ea44d2c549bce7497f0c55a2761aa84a51dfe

fix regression introduced by 258f9f5

tools.gd
@@ -61,7 +61,7 @@ func _change_mobile_emulation(val):
 	has_touch_screen = val
 	var action_caintainer_keyboard = get_node("Niveau/GUI/outGameGUI/HBoxContainer_droit/VBoxContainer/ActionsContainer")
 	var action_container_touch_droit = get_node("Niveau/GUI/outGameGUI/HBoxContainer_droit/VBoxContainer/ActionsContainerTouch")
-	var action_container_touch_overflow = get_node("Niveau/GUI/outGameGUI/HBoxContainer_gauche/VBoxContainer/ActionsContainerTouchOverflow/ActionsContainerTouch2")
+	var action_container_touch_overflow = get_node("Niveau/GUI/outGameGUI/ActionsContainerTouchOverflow/ActionsContainerTouch2")
 	var main_touch_controls = get_node("Niveau/GUI/touch_controls")
 	if Engine.editor_hint:
 		ProjectSettings.set_setting("input_devices/pointing/emulate_touch_from_mouse",val)